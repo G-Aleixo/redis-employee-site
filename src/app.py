1c600b1524e9dee78190108897de0ea459c9d9a1
G-Aleixo <163425479+G-Aleixo@users.noreply.github.com>
2026-01-22 01:19:04 +0000

    feat: add redis to requirement and fix some typing

--- src/app.py
+++ src/app.py
@@ -1,11 +1,18 @@
-from flask import Flask, request, make_response, jsonify, g, session, redirect, url_for
-from flask_cors import CORS
+from flask import Flask, request, make_response, jsonify, g, session, redirect, url_for # pyright: ignore[reportMissingImports]
+from flask_cors import CORS # pyright: ignore[reportMissingModuleSource]
 from markupsafe import escape
 from functools import wraps
 
 from sqlite3 import Row
 import src.database as database
 import hashlib
+
+from os import getenv
+
+REDIS = getenv("REDIS_URL") # setup manually
+
+if not REDIS:
+    print("REDIS_URL value not found in env, cache is disabled")
 
 DATABASE = "worker_database.db"
 
@@ -19,7 +26,7 @@ def stable_hash(data: any):
 
 def get_db() -> database.DatabaseConnection:
     if "db" not in g:
-        g.db = database.DatabaseConnection(DATABASE)
+        g.db = database.DatabaseConnection(DATABASE, REDIS)
         g.db.row_factory = Row
     return g.db
 
--- src/database.py
+++ src/database.py
@@ -1,5 +1,6 @@
 import sqlite3 as sql
 import hashlib
+import redis
 from datetime import datetime
 
 
@@ -10,27 +11,36 @@ def stable_hash(data: any):
 # Wrapper class for a db connection that implements a bunch of helper methods
 class DatabaseConnection:
     #TODO: use redis as a cache and introduce some delay
-    def __init__(self, database_path: str):
+    def __init__(self, database_path: str, redis_url: str):
         self.db: sql.Connection = sql.connect(database_path)
         self.db.row_factory = sql.Row
         self.cursor: sql.Cursor = self.db.cursor()
 
+        if redis_url:
+            try:
+                self.redis = redis.connect(redis_url)
+                self.cache_enabled = True
+            except:
+                self.cache_enabled = False
+        else:
+            self.cache_enabled = False
+
     def get_employee(self, employee_id: int):
-        query = "SELECT * FROM employee WHERE idEmployee = (?) LIMIT 1"
+        query = "SELECT * FROM employee WHERE idEmployee = (?) LIMIT 1;"
 
         return self.cursor.execute(query, (employee_id, )).fetchone()
 
     def get_employees(self):
-        return self.cursor.execute("SELECT * FROM employee").fetchall()
+        return self.cursor.execute("SELECT * FROM employee;").fetchall()
 
-    def employee_exists(self, employee_id) -> bool:
+    def employee_exists(self, employee_id: int) -> bool:
         query = "SELECT 1 FROM employee WHERE idEmployee = ?;"
         row = self.cursor.execute(query, (employee_id, )).fetchone()
         if row:
             return True
         return False
 
-    def add_employee(self, name, age: int, information, password: str, id_manager: int = None, favorite_team = "América Natal - RN"):
+    def add_employee(self, name: str, age: int, information: str, password: str, id_manager: int = None, favorite_team: str = "América Natal - RN"):
         exists = "SELECT 1 FROM employee WHERE name = ?;"
         row = self.cursor.execute(exists, (name, )).fetchone()
         if row:
@@ -53,7 +63,7 @@ class DatabaseConnection:
 
         return 200
     
-    def verify_password(self, employee_id, password):
+    def verify_password(self, employee_id: str, password: str):
         employee = self.cursor.execute("SELECT * FROM employee WHERE idEmployee = ?;", (employee_id, )).fetchone()
 
         if employee:
@@ -64,21 +74,23 @@ class DatabaseConnection:
 
         return self.cursor.execute(query, (task_id, )).fetchone()
     
-    def task_exists(self, task_id) -> bool:
+    def task_exists(self, task_id: int) -> bool:
         query = "SELECT 1 FROM workTask WHERE idWorkTask = ?;"
         row = self.cursor.execute(query, (task_id, )).fetchone()
         if row:
             return True
         return False
 
-    def add_task(self, name, content, done = False, done_time = None, project_id = None):
+    def add_task(self, name: str, content: str, done: bool = False, done_time: str = None, project_id: int = None):
         query = "INSERT INTO workTask (name, content, done, doneTimeDate, createdAt, idProject) VALUES (?, ?, ?, ?, ?, ?);"
         self.cursor.execute(query, (name, content, done, done_time, str(datetime.now()), project_id))
 
-    def get_tasks(self):
-        return self.cursor.execute("SELECT * FROM workTask").fetchall()
+        self.db.commit()
 
-    def mark_task_completed(self, task_id):
+    def get_tasks(self):
+        return self.cursor.execute("SELECT * FROM workTask;").fetchall()
+
+    def mark_task_completed(self, task_id: int):
         query = "UPDATE workTask SET done = TRUE WHERE idWorkTask = ?;"
 
         self.cursor.execute(query, (task_id, ))
@@ -93,6 +105,8 @@ class DatabaseConnection:
             query = "INSERT INTO assignment (idWorkTask, idEmployee) VALUES (?, ?);"
 
             self.cursor.execute(query, (task_id, employee_id))
+
+            self.db.commit()
 
     def get_assigned(self, task_id: int):
         query = "SELECT e.* FROM employee e INNER JOIN assignment a ON e.idEmployee = a.idEmployee WHERE a.idWorkTask = ?;"
@@ -115,6 +129,8 @@ class DatabaseConnection:
             assign_comment = "INSERT INTO taskComment (idComment, idWorkTask) VALUES (?, ?);"
 
             self.cursor.execute(assign_comment, (comment_id, task_id))
+
+            self.db.commit()
     
     def get_project(self, project_id: int):
         query = "SELECT * FROM project WHERE idProject = ?;"
@@ -131,7 +147,7 @@ class DatabaseConnection:
 
         return self.cursor.execute(query, (project_id, )).fetchall()
 
-    def add_project(self, name, manager_id):
+    def add_project(self, name: int, manager_id: int):
         if self.employee_exists(manager_id):
             query = "INSERT INTO project (name, idManager) VALUES (?, ?);"
 
@@ -139,7 +155,7 @@ class DatabaseConnection:
 
             self.db.commit()
 
-    def project_exists(self, project_id) -> bool:
+    def project_exists(self, project_id: int) -> bool:
         query = "SELECT 1 FROM project WHERE idProject = ?;"
         row = self.cursor.execute(query, (project_id, )).fetchone()
         if row:
@@ -162,6 +178,8 @@ class DatabaseConnection:
             assign_comment = "INSERT INTO projectComment (idComment, idProject) VALUES (?, ?);"
 
             self.cursor.execute(assign_comment, (comment_id, project_id))
+
+            self.db.commit()
 
 
     def close(self):
